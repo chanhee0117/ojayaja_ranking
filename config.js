b3db08205b0d203cc6221ab1e8d287d85931c093
chanhee0117 <chanhee0117@icloud.com>
2026-08-02 21:12:04 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const CONFIG={
   SHEET_ID:'11PnmT_BqMUp_l-oCgnbKR19bDUKgtiTznXNhuCG9hXg',
   SHEET_NAME:'1학기 자습 총시수',
   // Apps Script 웹 앱 배포 URL을 붙여넣으세요. 비워두면 이 브라우저에만 저장됩니다.
-  APPS_SCRIPT_URL:'https://script.google.com/macros/s/AKfycbzfH2Mi5k_OJIKeUau3Y5KKqVskKYxT3AMvkRinAyoDcnjtASVn7YSdAzFtu4OPIiCN/exec'
+  APPS_SCRIPT_URL:'https://script.google.com/macros/s/AKfycbw4_JDa3M4pGFOa3_P7vUnCDiDZbp6llsdt6bMQKaJ6CtPHHRI4cpzM_05RnMJQ50Ld/exec'
 };
